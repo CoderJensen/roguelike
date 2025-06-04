4bcc05a5cd30bad7d02ff72ac49dd08110ea07e2
Carter Jensen <carterajensen@outlook.com>
2025-06-03 22:07:46 -0500

    Added __main__

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from __future__ import annotations
 
+import tcod.context
+import tcod.tileset
+
 def main() -> None:
-    print("Hello world!")
+    """Load a tileset and open a window using it, this window will immediately close."""
+    tileset = tcod.tileset.load_tilesheet(
+        "data/Alloy_curses_12x12.png", columns=16, rows=16, charmap=tcod.tileset.CHARMAP_CP437
+    )
+    tcod.tileset.procedural_block_elements(tileset=tileset)
+    with tcod.context.new(tileset=tileset) as context:
+        # The window will stay open for the duration of the block
+        pass
 
 if __name__ == "__main__":
-    (main)
+    main()
